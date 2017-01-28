1f190a89e6263628cc77323377421c5ffe949d28
Michael McGahan <mike.mcgahan@gmail.com>
2017-01-28 22:26:58 +1300

    simplify error404

--- src/app/reducer.test.js
+++ src/app/reducer.test.js
@@ -1,16 +1,7 @@
-import {
-	DEFAULT_APP_STATE,
-	app,
-} from './reducer';
+import appReducers from './reducer';
 
 describe('reducer', () => {
-	beforeEach(function() {
-		this.MOCK_STATE = { foo: 'bar' };
-	});
-	it('returns default state for empty action', () => {
-		expect(app(undefined, {})).toEqual(DEFAULT_APP_STATE);
-	});
-	it('app data should be cleared by a LOGOUT_REQUEST event', function() {
-		expect(app(this.MOCK_STATE, { type: 'LOGOUT_REQUEST' })).toEqual(DEFAULT_APP_STATE);
+	it('returns an object', () => {
+		expect(appReducers).toEqual(jasmine.any(Object));
 	});
 });
